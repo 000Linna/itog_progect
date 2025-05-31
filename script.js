fabedd57ee33e9a4d37a15dacf3b116491a87346
000Linna <147407037+000Linna@users.noreply.github.com>
2025-05-31 14:23:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,43 +3,44 @@ const nameInput = document.querySelector('#operationName');
 const list = document.querySelector('#list');
 const sum = document.querySelector('#sum')
 const category = document.querySelector('#category')
-const sumAll = document.querySelector('.sumAll')
+const sumAll = document.querySelector('#sumAll')
+const price = document.querySelector('#sum')
 let total = 0;
-addBtn.addEventListener('click', () => {
-    // Создание элемента "li" в памяти (на странице его пока что нет)
-    const newItem = document.createElement('li');
-    // Добавить элементу newItem класс item (элемент еще не на странице)
-    newItem.classList.add('item');
-    // Добавить элементу текстовое содержимое из поля ввода
-    newItem.textContent = nameInput.value + "" + sum.value
-    newItem.innerHTML = `
+const
+    addBtn.addEventListener('click', () => {
+        // Создание элемента "li" в памяти (на странице его пока что нет)
+        const newItem = document.createElement('li');
+        // Добавить элементу newItem класс item (элемент еще не на странице)
+        newItem.classList.add('item');
+        // Добавить элементу текстовое содержимое из поля ввода
+        newItem.textContent = nameInput.value + "" + sum.value
+        newItem.innerHTML = `
         <li>
                 <h2 class="name">${nameInput.value}</h2>
                 <p class="categoty">${category.value}</p>
                 <p class="price">${sum.value}</p>
             </li>`
-    // Добавить элемент в список
-    list.appendChild(newItem);
-total=total+PromiseRejectionEvent.value;
-sumAll.textContent.total
+        // Добавить элемент в список
+        list.appendChild(newItem);
+        total = total + +sum.value;
+        sumAll.textContent = total
 
-    // Отчистить поле ввода
-    nameInput.value = '';
+        // Отчистить поле ввода
+        nameInput.value = '';
 
-    // creat button
-    const deleteButton = document.createElement("button");
-    // добавляем текст кнопке
-    deleteButton.textContent = 'delete'
-    // добавить кнопку в элимент списка
-    newItem.appendChild(deleteButton);
+        // creat button
+        const deleteButton = document.createElement("button");
+        // добавляем текст кнопке
+        deleteButton.textContent = 'delete'
+        // добавить кнопку в элимент списка
+        newItem.appendChild(deleteButton);
 
-    deleteButton.addEventListener('click', () => {
-total=total- +newItem.querySelector('.price').textContent
-console.log(total)
-        // удаление элимента из списка
-        list.removeChild(newItem);
+        deleteButton.addEventListener('click', () => {
+            total = total - +newItem.querySelector('.price').textContent
+            // удаление элимента из списка
+            list.removeChild(newItem);
+        })
     })
-})
 
 
 
